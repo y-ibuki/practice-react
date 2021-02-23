7859226e8ea41e7e3df42af84ed3f1fea1d03bf3
y-ibuki <y.ibuki91@gmail.com>
2021-02-23 19:25:32 +0900

    first commit
    修正
    
    gitignore修正

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+// import Labeldom from './components/label'
+// import Highcharts from 'highcharts';
+// import HighchartsReact from 'highcharts-react-official';
+import axios from "axios";
 
 function App() {
+  const prefectureUrl = 'https://opendata.resas-portal.go.jp/api/v1/prefectures';
+  const populationUrl = 'https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear'
+  const apiKey = process.env.REACT_APP_API_KEY;
+  // 都道府県取得
+  const prefectures = axios.get( prefectureUrl, { headers: { 'X-API-KEY': apiKey } })
+  // 都道府県ごとに人口取得
+  const populationByPrefecture = axios.get( populationUrl, { headers: { 'X-API-KEY': apiKey } })
+  const [pref, setPref] = useState([])
+  useEffect( () => {
+    const allPref = [];
+    prefectures.then( res => {
+      const prefData = res.data.result
+      for (let pref of prefData) {
+        allPref.push(pref)
+      }
+    })
+    setPref(allPref)
+    console.log(allPref)
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {
+          pref.map( (name, index) => {
+            console.log(name)
+            return ('test')
+          })
+        }
+      {/*<HighchartsReact highcharts={Highcharts} options={options}></HighchartsReact>*/}
     </div>
-  );
+  )
 }
 
 export default App;
